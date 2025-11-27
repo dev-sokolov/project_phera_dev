87c2cd1d8fb9685805b7f26e854c7523a0585d86
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-11-27 15:05:25 +0100

    fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,19 @@
 import styles from "./Button.module.css";
 
 const Button = ({ onClick, children }) => {
+      const handleClick = (e) => {
+        // убрать pressed-state на iOS
+        e.currentTarget.classList.add("no-active");
+
+        requestAnimationFrame(() => {
+            e.currentTarget.classList.remove("no-active");
+        });
+
+        onClick?.(e);
+    };
+
     return (
-        <button type="button" onClick={onClick} className={styles.btn}>
+        <button type="button" onClick={handleClick} className={styles.btn}>
             {children}
         </button>
     )
