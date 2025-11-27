38a925eb6ff1941606f95414bedf40c6aba64098
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-11-27 15:07:08 +0100

    fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,26 @@
 import styles from "./Button.module.css";
 
 const Button = ({ onClick, children }) => {
-      const handleClick = (e) => {
-        // убрать pressed-state на iOS
-        e.currentTarget.classList.add("no-active");
+    //   const handleClick = (e) => {
+    //     // убрать pressed-state на iOS
+    //     e.currentTarget.classList.add("no-active");
 
-        requestAnimationFrame(() => {
-            e.currentTarget.classList.remove("no-active");
-        });
+    //     requestAnimationFrame(() => {
+    //         e.currentTarget.classList.remove("no-active");
+    //     });
 
+    //     onClick?.(e);
+    // };
+    const handleClick = (e) => {
         onClick?.(e);
+
+        // сброс активного состояния
+        e.target.style.webkitTapHighlightColor = "transparent";
+
+        // на следующем кадре убрать inline-style
+        requestAnimationFrame(() => {
+            e.target.style.webkitTapHighlightColor = "";
+        });
     };
 
     return (
