22b639249730edde09945e3a986e11df1ace0617
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-12-03 20:44:28 +0100

    fix

--- src/pages/ResultWithoutDetailsPage/ResultWithoutDetailsPage.jsx
+++ src/pages/ResultWithoutDetailsPage/ResultWithoutDetailsPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import Container from "../../components/Container/Container";
