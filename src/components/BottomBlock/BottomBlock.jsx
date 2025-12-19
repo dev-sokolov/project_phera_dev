63a21301ca5cd98a8544640ea4c9a193d9c098e2
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-12-19 21:43:26 +0100

    fix

--- src/components/BottomBlock/BottomBlock.jsx
+++ src/components/BottomBlock/BottomBlock.jsx
@@ -1,15 +1,32 @@
+import { motion } from "framer-motion";
 import styles from "./BottomBlock.module.css";
+
+// const BottomBlock = ({ children }) => {
+//     return (
+//         <>
+//             <div className={styles.bottomBlock}>
+//                 <div className={styles.bottomBlockInner}>
+//                     {children}
+//                 </div>
+//             </div>
+//         </>
+//     )
+// };
+
+// export default BottomBlock;
 
 const BottomBlock = ({ children }) => {
     return (
-        <>
-            <div className={styles.bottomBlock}>
-                <div className={styles.bottomBlockInner}>
-                    {children}
-                </div>
-            </div>
-        </>
-    )
+        <motion.div
+            initial={{ opacity: 0, y: 20 }}     // старт: чуть ниже и прозрачный
+            animate={{ opacity: 1, y: 0 }}      // финал: полностью видно
+            exit={{ opacity: 0, y: 20 }}        // при выходе: снова вниз и исчезает
+            transition={{ duration: 0.3, ease: "easeInOut" }} // плавность
+            className={styles.bottomBlock}
+        >
+            <div className={styles.bottomBlockInner}>{children}</div>
+        </motion.div>
+    );
 };
 
 export default BottomBlock;
