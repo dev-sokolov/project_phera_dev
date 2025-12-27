fbe05536d175aacbfd49d6e5b62689663e697665
Ruslan.Sokolov <vaquero1master@gmail.com>
2025-12-27 20:36:05 +0100

    fix

--- src/pages/CameraCapture/CameraCapture.jsx
+++ src/pages/CameraCapture/CameraCapture.jsx
@@ -1,334 +1,148 @@
-// import { useRef, useState, useEffect } from "react";
-// import Webcam from "react-webcam";
-// import { useCameraReady } from "../../hooks/useCameraReady";
-// import { useMarkerDetection } from "../../hooks/useMarkerDetection";
-// import { useNavigate } from "react-router-dom";
-// // import AdjustableFrame from "./AdjustableFrame";
+import { useRef, useState, useEffect } from "react";
+import Webcam from "react-webcam";
+import { useCameraReady } from "../../hooks/useCameraReady";
+import { useMarkerDetection } from "../../hooks/useMarkerDetection";
+import { useNavigate } from "react-router-dom";
+// import AdjustableFrame from "./AdjustableFrame";
 
-// import whiteFrame from "../../assets/whiteFrame.svg";
-// import greenFrame from "../../assets/greenFrame.svg";
-// import Logo from "../../assets/Logo";
-// import ArrowLeft from "../../assets/icons/ArrowLeft";
+import whiteFrame from "../../assets/whiteFrame.svg";
+import greenFrame from "../../assets/greenFrame.svg";
+import Logo from "../../assets/Logo";
+import ArrowLeft from "../../assets/icons/ArrowLeft";
 
-// import styles from "./CameraCapture.module.css";
+import styles from "./CameraCapture.module.css";
 
-// const CameraCapture = ({ onCapture, onExit }) => {
-//     const webcamRef = useRef(null);
-//     const videoRef = useRef(null);
-//     const frameRef = useRef(null);
-//     const navigate = useNavigate();
+const CameraCapture = ({ onCapture, onExit }) => {
+    const webcamRef = useRef(null);
+    const videoRef = useRef(null);
+    const frameRef = useRef(null);
+    const navigate = useNavigate();
 
-//     const [isInside, setInside] = useState(false);
-//     const [insideTimer, setInsideTimer] = useState(null);
+    const [isInside, setInside] = useState(false);
+    const [insideTimer, setInsideTimer] = useState(null);
 
-//     const isReady = useCameraReady(webcamRef);
+    const isReady = useCameraReady(webcamRef);
 
-//     const videoConstraints = {
-//         facingMode: "environment",
-//         width: { ideal: 1920 },
-//         height: { ideal: 1080 },
-//     };
-
-//     useEffect(() => {
-//         if (isInside) {
-//             // Если таймер уже существует — ничего не делаем
-//             if (insideTimer) return;
-
-//             // Запускаем таймер на 2 секунды
-//             const timer = setTimeout(() => {
-//                 navigate("/camera-processing", { replace: true });  // ← переход
-//             }, 2000);
-
-//             setInsideTimer(timer);
-//         } else {
-//             // Если объект вышел из рамки — удаляем таймер
-//             if (insideTimer) {
-//                 clearTimeout(insideTimer);
-//                 setInsideTimer(null);
-//             }
-//         }
-//     }, [isInside]);
-
-//     // Останавливаем камеру при размонтировании
-//     useEffect(() => {
-//         return () => {
-//             const video = webcamRef.current?.video;
-//             video?.srcObject?.getTracks().forEach((track) => track.stop());
-//         };
-//     }, []);
-
-//     // const handleExit = () => {
-//     //     const video = webcamRef.current?.video;
-//     //     video?.srcObject?.getTracks().forEach((track) => track.stop());
-//     //       onExit?.();
-//     // };
-
-//     // useMarkerDetection(videoRef, frameRef, setInside); //временно !!!! убрать комментирование!!!!!!!!!!!!
-
-//     useEffect(() => {                        //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//         const timer1 = setTimeout(() => {       //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//             setInside(true);                    //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//         }, 3000);                               //временно !!!!!!!! Удалить!!!!!!!!!!!!
-
-//         const timer2 = setTimeout(() => {       //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//             navigate("/camera-processing", { replace: true });     //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//         }, 4000);                               //временно !!!!!!!! Удалить!!!!!!!!!!!!
-
-//         return () => {                          //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//             clearTimeout(timer1);               //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//             clearTimeout(timer2);               //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//         };                                      //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//     }, []);                                     //временно !!!!!!!! Удалить!!!!!!!!!!!!
-
-//     // useEffect(() => {
-//     //     const handlePopState = () => {
-//     //         navigate("/camera-access", { replace: true });
-//     //     };
-
-//     //     window.addEventListener("popstate", handlePopState);
-//     //     return () => window.removeEventListener("popstate", handlePopState);
-//     // }, [navigate]);
-
-//     const Frame = ({ inside }) => (
-//         <img
-//             src={inside ? greenFrame : whiteFrame}
-//             className={styles.frameImage}
-//             alt="frame"
-//         />
-//     );
-
-//     const buttonContent = isInside ? "Successfully captured" : "Move closer";
-
-//     return (
-//         <div className={styles.content}>
-//             <div className={styles.containerInner}>
-//                 <div className={styles.cameraBlock}>
-//                     <div className={styles.cameraContainer}>
-//                         <div className={styles.overlay}>
-//                             <button
-//                                 className={`${styles.btn} ${isInside ? styles.btnSuccess : ""}`}
-//                             >
-//                                 {buttonContent}
-//                             </button>
-//                             <div ref={frameRef} className={styles.frameBox}>
-//                                 <Frame inside={isInside} />
-//                                 {/* <AdjustableFrame /> */}
-//                             </div>
-//                         </div>
-
-//                         <Webcam
-//                             ref={webcamRef}
-//                             audio={false}
-//                             screenshotFormat="image/png"
-//                             videoConstraints={videoConstraints}
-//                             className={`${styles.webcamVideo} ${isReady ? styles.show : ""}`}
-//                             onUserMedia={() => {
-//                                 videoRef.current = webcamRef.current.video;
-//                             }}
-//                             onUserMediaError={(err) => {
-//                                 console.error("Camera error:", err);
-//                                 alert("Unable to start camera. Check permissions.");
-//                                 onExit();
-//                             }}
-//                             playsInline
-//                         />
-//                     </div>
-//                 </div>
-
-//                 <div className={styles.bottomBlock}>
-//                     <p className={styles.text}>
-//                         Place your test strip inside the frame and hold still — we’ll scan it automatically.
-//                     </p>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default CameraCapture;
-
-
-
-
-
-
-// import { useRef, useState, useEffect } from "react";
-// import Webcam from "react-webcam";
-// import { useCameraReady } from "../../hooks/useCameraReady";
-// import { useMarkerDetection } from "../../hooks/useMarkerDetection";
-// import { useNavigate } from "react-router-dom";
-// // import AdjustableFrame from "./AdjustableFrame";
-
-// import whiteFrame from "../../assets/whiteFrame.svg";
-// import greenFrame from "../../assets/greenFrame.svg";
-// import Logo from "../../assets/Logo";
-// import ArrowLeft from "../../assets/icons/ArrowLeft";
-
-// import styles from "./CameraCapture.module.css";
-
-// const CameraCapture = ({ onCapture, onExit }) => {
-//     const webcamRef = useRef(null);
-//     const videoRef = useRef(null);
-//     const frameRef = useRef(null);
-//     const navigate = useNavigate();
-
-//     const [isInside, setInside] = useState(false);
-//     const [insideTimer, setInsideTimer] = useState(null);
-
-//     const isReady = useCameraReady(webcamRef);
-
-//     const videoConstraints = {
-//         facingMode: "environment",
-//         width: { ideal: 1920 },
-//         height: { ideal: 1080 },
-//     };
-
-//     useEffect(() => {
-//         if (isInside) {
-//             // Если таймер уже существует — ничего не делаем
-//             if (insideTimer) return;
-
-//             // Запускаем таймер на 2 секунды
-//             const timer = setTimeout(() => {
-//                 navigate("/camera-processing", { replace: true });  // ← переход
-//             }, 2000);
-
-//             setInsideTimer(timer);
-//         } else {
-//             // Если объект вышел из рамки — удаляем таймер
-//             if (insideTimer) {
-//                 clearTimeout(insideTimer);
-//                 setInsideTimer(null);
-//             }
-//         }
-//     }, [isInside]);
-
-//     // Останавливаем камеру при размонтировании
-//     useEffect(() => {
-//         return () => {
-//             const video = webcamRef.current?.video;
-//             video?.srcObject?.getTracks().forEach((track) => track.stop());
-//         };
-//     }, []);
-
-//     // const handleExit = () => {
-//     //     const video = webcamRef.current?.video;
-//     //     video?.srcObject?.getTracks().forEach((track) => track.stop());
-//     //       onExit?.();
-//     // };
-
-//     // useMarkerDetection(videoRef, frameRef, setInside); //временно !!!! убрать комментирование!!!!!!!!!!!!
-
-//     useEffect(() => {                        //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//         const timer1 = setTimeout(() => {       //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//             setInside(true);                    //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//         }, 3000);                               //временно !!!!!!!! Удалить!!!!!!!!!!!!
-
-//         const timer2 = setTimeout(() => {       //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//             navigate("/camera-processing", { replace: true });     //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//         }, 4000);                               //временно !!!!!!!! Удалить!!!!!!!!!!!!
-
-//         return () => {                          //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//             clearTimeout(timer1);               //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//             clearTimeout(timer2);               //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//         };                                      //временно !!!!!!!! Удалить!!!!!!!!!!!!
-//     }, []);                                     //временно !!!!!!!! Удалить!!!!!!!!!!!!
-
-//     // useEffect(() => {
-//     //     const handlePopState = () => {
-//     //         navigate("/camera-access", { replace: true });
-//     //     };
-
-//     //     window.addEventListener("popstate", handlePopState);
-//     //     return () => window.removeEventListener("popstate", handlePopState);
-//     // }, [navigate]);
-
-//     const Frame = ({ inside }) => (
-//         <img
-//             src={inside ? greenFrame : whiteFrame}
-//             className={styles.frameImage}
-//             alt="frame"
-//         />
-//     );
-
-//     const buttonContent = isInside ? "Successfully captured" : "Move closer";
-
-//     return (
-//         <div className={styles.content}>
-//             <div className={styles.containerInner}>
-//                 <div className={styles.cameraBlock}>
-//                     <div className={styles.cameraContainer}>
-//                         <div className={styles.overlay}>
-//                             <button
-//                                 className={`${styles.btn} ${isInside ? styles.btnSuccess : ""}`}
-//                             >
-//                                 {buttonContent}
-//                             </button>
-//                             <div ref={frameRef} className={styles.frameBox}>
-//                                 <Frame inside={isInside} />
-//                                 {/* <AdjustableFrame /> */}
-//                             </div>
-//                         </div>
-
-//                         <Webcam
-//                             ref={webcamRef}
-//                             audio={false}
-//                             screenshotFormat="image/png"
-//                             videoConstraints={videoConstraints}
-//                             className={`${styles.webcamVideo} ${isReady ? styles.show : ""}`}
-//                             onUserMedia={() => {
-//                                 videoRef.current = webcamRef.current.video;
-//                             }}
-//                             onUserMediaError={(err) => {
-//                                 console.error("Camera error:", err);
-//                                 alert("Unable to start camera. Check permissions.");
-//                                 onExit();
-//                             }}
-//                             playsInline
-//                         />
-//                     </div>
-//                 </div>
-
-//                 <div className={styles.bottomBlock}>
-//                     <p className={styles.text}>
-//                         Place your test strip inside the frame and hold still — we’ll scan it automatically.
-//                     </p>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default CameraCapture;
-
-
-
-import ArucoCamera from "../../hooks/ArucoCamera";
-import TestAruco from "../../hooks/TestAruco";
-import { useEffect } from "react";
-
-const CameraCapture = () => {
+    const videoConstraints = {
+        facingMode: "environment",
+        width: { ideal: 1920 },
+        height: { ideal: 1080 },
+    };
 
     useEffect(() => {
-        const onCvReady = () => {
-            console.log("OpenCV загружен!");
-            startCamera();    // запускаем камеру
-            testMarker();     // тест на фото
-        };
+        if (isInside) {
+            // Если таймер уже существует — ничего не делаем
+            if (insideTimer) return;
 
-        if (cv?.onRuntimeInitialized) {
-            cv.onRuntimeInitialized = onCvReady;
-        } else if (cv) {
-            onCvReady();
+            // Запускаем таймер на 2 секунды
+            const timer = setTimeout(() => {
+                navigate("/camera-processing", { replace: true });  // ← переход
+            }, 2000);
+
+            setInsideTimer(timer);
+        } else {
+            // Если объект вышел из рамки — удаляем таймер
+            if (insideTimer) {
+                clearTimeout(insideTimer);
+                setInsideTimer(null);
+            }
         }
+    }, [isInside]);
+
+    // Останавливаем камеру при размонтировании
+    useEffect(() => {
+        return () => {
+            const video = webcamRef.current?.video;
+            video?.srcObject?.getTracks().forEach((track) => track.stop());
+        };
     }, []);
 
-    return (
-        <div>
-            <ArucoCamera />
-            <TestAruco />
-        </div>
-    )
-}
+    // const handleExit = () => {
+    //     const video = webcamRef.current?.video;
+    //     video?.srcObject?.getTracks().forEach((track) => track.stop());
+    //       onExit?.();
+    // };
 
-export default CameraCapture
+    // useMarkerDetection(videoRef, frameRef, setInside); //временно !!!! убрать комментирование!!!!!!!!!!!!
+
+    useEffect(() => {                        //временно !!!!!!!! Удалить!!!!!!!!!!!!
+        const timer1 = setTimeout(() => {       //временно !!!!!!!! Удалить!!!!!!!!!!!!
+            setInside(true);                    //временно !!!!!!!! Удалить!!!!!!!!!!!!
+        }, 3000);                               //временно !!!!!!!! Удалить!!!!!!!!!!!!
+
+        const timer2 = setTimeout(() => {       //временно !!!!!!!! Удалить!!!!!!!!!!!!
+            navigate("/camera-processing", { replace: true });     //временно !!!!!!!! Удалить!!!!!!!!!!!!
+        }, 4000);                               //временно !!!!!!!! Удалить!!!!!!!!!!!!
+
+        return () => {                          //временно !!!!!!!! Удалить!!!!!!!!!!!!
+            clearTimeout(timer1);               //временно !!!!!!!! Удалить!!!!!!!!!!!!
+            clearTimeout(timer2);               //временно !!!!!!!! Удалить!!!!!!!!!!!!
+        };                                      //временно !!!!!!!! Удалить!!!!!!!!!!!!
+    }, []);                                     //временно !!!!!!!! Удалить!!!!!!!!!!!!
+
+    // useEffect(() => {
+    //     const handlePopState = () => {
+    //         navigate("/camera-access", { replace: true });
+    //     };
+
+    //     window.addEventListener("popstate", handlePopState);
+    //     return () => window.removeEventListener("popstate", handlePopState);
+    // }, [navigate]);
+
+    const Frame = ({ inside }) => (
+        <img
+            src={inside ? greenFrame : whiteFrame}
+            className={styles.frameImage}
+            alt="frame"
+        />
+    );
+
+    const buttonContent = isInside ? "Successfully captured" : "Move closer";
+
+    return (
+        <div className={styles.content}>
+            <div className={styles.containerInner}>
+                <div className={styles.cameraBlock}>
+                    <div className={styles.cameraContainer}>
+                        <div className={styles.overlay}>
+                            <button
+                                className={`${styles.btn} ${isInside ? styles.btnSuccess : ""}`}
+                            >
+                                {buttonContent}
+                            </button>
+                            <div ref={frameRef} className={styles.frameBox}>
+                                <Frame inside={isInside} />
+                                {/* <AdjustableFrame /> */}
+                            </div>
+                        </div>
+
+                        <Webcam
+                            ref={webcamRef}
+                            audio={false}
+                            screenshotFormat="image/png"
+                            videoConstraints={videoConstraints}
+                            className={`${styles.webcamVideo} ${isReady ? styles.show : ""}`}
+                            onUserMedia={() => {
+                                videoRef.current = webcamRef.current.video;
+                            }}
+                            onUserMediaError={(err) => {
+                                console.error("Camera error:", err);
+                                alert("Unable to start camera. Check permissions.");
+                                onExit();
+                            }}
+                            playsInline
+                        />
+                    </div>
+                </div>
+
+                <div className={styles.bottomBlock}>
+                    <p className={styles.text}>
+                        Place your test strip inside the frame and hold still — we’ll scan it automatically.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CameraCapture;
